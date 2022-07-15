82de58dc084f0634464f87d3031c8959745700dd
ajanaantony089@gmail.com <ajanaantony089@gmail.com>
2022-07-15 17:59:39 +0400

    commit

--- veg.js
+++ veg.js
@@ -1,15 +1,14 @@
-
-          var people=[];
-                // creating an object
+  
+               
              var xhttp=new XMLHttpRequest();
-             xhttp.onreadystatechange=function(){
+             xhttp.onreadystatechange=function(e){
                 if(this.readyState==4&&this.status==200){
-                    var response=JSON.parse(this.responseText);
-                    people=response;
-                    console.log(people);
-                    buildTable(people);
-                   
-                }
+                     response=JSON.parse(this.responseText);
+                     buildTable(response);
+                     //selectvalue(response) ;    
+                    getdata(response);
+                     
+        }
             }
             xhttp.open("get","veg.json",true);
             xhttp.send();
@@ -18,9 +17,12 @@
 
             function buildTable(data){
                 var table = document.getElementById('myTable')
-        
-                for (var i = 0; i < data.length; i++){
+                
+                for (var i = 0; i < data.length; i++)
+                {
+                   
                     var row = `<tr>
+                
                                     <td>${data[i].SerialNumber}</td>
                                     <td>${data[i].Item}</td>
                                     <td>${data[i].unit}</td>
@@ -28,9 +30,88 @@
                                     <td>${data[i].Note}</td>
                                     <td>${data[i].Department}</td>
                               </tr>`
-                    table.innerHTML += row
-        
-        
+                             
+                              table.innerHTML += row
+              }
+            
+      }
+          
+         
+
+          
+              
+
+               function getvalue(){
+                  var selected=document.getElementById("arr").value;
+                    return selected;
                 }
-            }
         
+               
+             function   getdata(response){
+              var data=response;
+              return data;
+             }
+
+
+             function deptTable(data){
+              var depttable = document.getElementById('depttable')
+              
+              for (var i = 0; i < data.length; i++)
+              {
+                 
+                  var row = `<tr>
+              
+                                  <td>${data[i].SerialNumber}</td>
+                                  <td>${data[i].Item}</td>
+                                  <td>${data[i].unit}</td>
+                                  <td>${data[i].Quantity}</td>
+                                  <td>${data[i].Note}</td>
+                                  <td>${data[i].Department}</td>
+                            </tr>`
+                           
+                           depttable.innerHTML += row;
+            }
+          
+    }
+                  
+
+        
+           function filtering() {
+              
+            var value=document.getElementById("arr").value;
+           var table = document.getElementById('myTable');
+           var depttable = document.getElementById('myTable')
+            if(value==='ALL'){
+            
+              table.setAttribute("hidden","true");
+              deptTable(response);
+              
+            }
+           
+           var output=response.filter(function (e) {
+            
+                var x= e.Department.includes(value);
+                return x;
+         })
+             
+         console.table(output);
+        table.setAttribute("hidden","true");
+     
+         deptTable(output); 
+
+
+             
+
+          }
+
+
+
+
+        
+                  
+                 
+                  
+        
+        
+                
+              
